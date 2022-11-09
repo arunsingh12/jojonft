41d94736ed808b01af08cd03786a5857c602594a
arunsingh12 <aruns7010@gmail.com>
2022-11-09 19:04:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import Layout from './components/Layouts/Layout'
 
 function App() {
   return <Layout/>;
-  // return(
-  //   <div>
-  //     <h1>
-  //       Hello  My Name is Demo
-  //     </h1>
-
-  //   </div>
-  // )
+ 
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 // import Routers from '../../routes/Router'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className='header'>
+      
+    </header>
   )
 }
 
